4b14f2eaad0509e90f52be50224e2e5c5dee9856
high-mountains <highmountain430@gmail.com>
2025-02-05 20:39:32 +0900

    [fix] complete leftSidebar_2-highmountain430-2025/02/05 20-39

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
 import { Outlet } from "react-router-dom";
 import { CgPokemon, CgCalendar,  } from "react-icons/cg";
 import { BsBuilding } from "react-icons/bs";
-import { Layout, Menu, theme, Grid } from 'antd';
+import { Layout, Menu, theme, Grid, Button } from 'antd';
 import { UnorderedListOutlined } from '@ant-design/icons';
 import styles from './index.module.scss';
 const { Header, Sider, Content } = Layout;
@@ -12,53 +15,58 @@ const { useBreakpoint } = Grid;
 
 const LayoutSide = () => {
   const screens = useBreakpoint();
+  const [responsive, setResponsive] = useState(false);
   const [collapsed, setCollapsed] = useState(false);
+  const [sidebarPresentStatus, setSidebarPresentStatus] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
   useEffect(() => {
-    // Will collapse when screen is 'md' (768px) or smaller
-    setCollapsed(!screens.md);
+    setResponsive(!screens.md);
+    if (!screens.md) setCollapsed(true);
   }, [screens.md]);
+
+  useEffect(() => {
+    
+  }, [screens.md])
+
   return (
     <Layout>
-      <Header
+      <div
         style={{
           background: colorBgContainer,
         }}
         className={`${styles.header}`}
       >
-        <div className={`${collapsed ? styles.logoContainerAlone : styles.logoContainer}`}>
+        <div className={`${responsive ? styles.logoContainerAlone : styles.logoContainer}`}>
           <img className={styles.logoImage} src="./assets/images/pokemon.png" alt="pokemon.png" />
           <span className={`${styles.logoText}`}>
             Pokemon
           </span>
         </div>
         
-        <div className={`${collapsed ? styles.logoContainer : styles.logoContainerAlone}`}>
-          <UnorderedListOutlined />
-          {/* <img className={styles.logoImage} src="./assets/images/pokemon.png" alt="pokemon.png" />
-          <span className={`${styles.logoText}`}>
-            Pokemon
-          </span> */}
+        <div className={`${responsive ? styles.logoContainer : styles.logoContainerAlone}`}>
+          <UnorderedListOutlined onClick={() => setSidebarPresentStatus(!sidebarPresentStatus)}/>
+          {/* <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setSidebarPresentStatus(!sidebarPresentStatus)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+              className={`${responsive ? styles.none : styles.show}`}
+            /> */}
         </div>
-        {/* <Button
-          type="text"
-          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          onClick={() => setCollapsed(!collapsed)}
-          style={{
-            fontSize: '16px',
-            backgroundColor: 'red',
-          }}
-        /> */}
-      </Header>
+      </div>
       <Layout
        style={{
         height: '100%',
         marginTop: '64px',
         }}>  
-        <Sider trigger={null} collapsible collapsed={collapsed} className={`${styles.siderWrapper}`}>
+        <Sider trigger={null} collapsible collapsed={collapsed} className={`${styles.leftSiderWrapper} ${sidebarPresentStatus ? styles.leftSidebarNone : styles.leftSidebarShow}`}>
           <Menu
             theme="dark"
             mode="inline"
@@ -80,9 +88,26 @@ const LayoutSide = () => {
                 label: 'シティ結果(日別)',
               },
             ]}
+            className={`${styles.menuTag}`}
           />
+          <div className='tag'>
+
+          </div>
         </Sider>
         <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+              className={`${responsive ? styles.none : styles.show}`}
+            />
+          </Header>
           <Content
             style={{
               margin: '24px 16px',
