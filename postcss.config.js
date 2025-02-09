c32382420f302af13f4a2cecc95e277f458145fe
high-mountains <highmountain430@gmail.com>
2025-02-09 09:17:46 +0900

    [fix] deploy setting fix-highmountain430-2025/02/09 09-18

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: [
+    require('postcss-flexbugs-fixes'),
     require('tailwindcss'),
     require('autoprefixer'),
-  ]
-}
+    require('postcss-preset-env')({
+      stage: 3, // Optional: Define the stage for CSS feature support
+      features: {
+        'custom-properties': false, // Optional: Disable specific features if not needed
+      },
+    }),
+  ],
+};
